639548be141dc3fcf0127d0d5a564a885788db10
PabloLUC9832 <lucaspablo340@outlook.com>
2022-11-24 07:51:45 -0600

    Creacion del proyecto

---
+++ src/administrador/FXMLFormularioAltaAdministradorController.java
@@ -0,0 +1,17 @@
+package administrador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class FXMLFormularioAltaAdministradorController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
